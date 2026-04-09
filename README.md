bb1435568bd46b43931bb6b8a521887ed391db5f
Maksim Tomkavich <quendimax@gmail.com>
2026-04-09 21:21:59 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,24 @@
+# preassakey
+
+## Why this app exists?
+
+There is no a crossplatform **Press any key** tool that would be possible to use
+with any shell you prefer (at least I couldn't find that kind of tool). Now in
+different shells different tools/commands are used, e.g. `read` in bash and
+fish, `pause` in cmd.exe, etc. But even `read` in different shells has different
+behaviour of its parameters.
+
+So, I wrote this very primitive app that behaves in the same way on all
+available platforms.
+
+## How to use?
+
+Just run the app in your terminal, and it will print the message `Press any
+key...`. After you press any key (except for modifier keys), the app will return
+control to your shell.
+
+- `-p, --prompt` option allows you to change the message.
+- `-e, --expect` specifies the symbols typing that the app return controll to
+  the shell. Note that the symbols depend on you language layout; thay are not
+  key or scan codes.
+- `-s, --silent` allow to suppress the prompt message entirely.
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use argh::{self, FromArgs};
+use crossterm::{event, terminal};
+use std::io;
+
+/// Simple crossplatform program that is waiting until a key is pressed
+#[derive(FromArgs)]
+struct Args {
+    /// expected keys to press to exit; if skipped, any key is expected
+    #[argh(option, short = 'e')]
+    expect: Option<String>,
+
+    /// a help message for user
+    #[argh(option, short = 'p')]
+    prompt: Option<String>,
+
+    /// suppress any prompt printing, even a new line
+    #[argh(switch, short = 's')]
+    silent: bool,
+}
+
+fn main() -> io::Result<()> {
+    let args: Args = argh::from_env();
+    if !args.silent {
+        if let Some(prompt) = args.prompt {
+            println!("{prompt}");
+        } else {
+            println!("Press any key...");
+        }
+    }
+
+    terminal::enable_raw_mode()?;
+    loop {
+        let ev = event::read()?;
+        if let Some(key_event) = ev.as_key_event() {
+            match (&args.expect, key_event.code.as_char()) {
+                (Some(expect), Some(input)) if expect.contains(input) => break,
+                (None, ..) => break,
+                _ => continue,
+            }
+        }
+    }
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
